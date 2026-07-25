008ec6f27d2d01889b6d4e53a50a19094634e66a
udaykashyap190-max <udaykashyap190@gmail.com>
2026-07-25 21:28:09 +0530

    Update BOT_TOKEN to use os.getenv
    Refactor BOT_TOKEN to use environment variable.

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
-BOT_TOKEN = "8918139671:AAF47eoBSjOl0KzVwyfv1BvwLyMTNrpWVPg"
+import os
 
-OWNER_ID = 8396339706
+
+BOT_TOKEN = os.getenv(
+    "8918139671:AAF47eoBSjOl0KzVwyfv1BvwLyMTNrpWVPg",
+    ""
+)
+
+
+ADMIN_IDS = [
+
+    8396339706
+
+]
